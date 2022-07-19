960a0e833b867456b220046be3a4cb670757422f
Kieren Anderson <kjander0@gmail.com>
2022-07-19 19:47:40 +1000

    Initial commit

---
+++ desktop/src/com/gmail/kjander0/tankcity/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.gmail.kjander0.tankcity;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.gmail.kjander0.tankcity.Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Tank City");
+		new Lwjgl3Application(new Game(), config);
+	}
+}
